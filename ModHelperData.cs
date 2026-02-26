abad5c3fdfc7ede64e0a4573464f0404b6a43cd0
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-02-26 11:38:22 -0700

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace MarbleMonkey;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "52.0";
+    public const string Version = "1.0.0";
+    public const string Name = "MarbleMonkeyMod";
+
+    public const string Description = "Adds a new tower to the game: The Marble Monkey!";
+
+    public const string RepoOwner = "RedBlooN-YT";    
+    public const string RepoName = "MarbleMonkey";
+}
