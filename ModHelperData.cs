e6e174ca8c51852b376639008b57e4f72d461be9
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-03-25 08:15:01 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,9 +2,9 @@ namespace MarbleMonkey;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "52.0";
-    public const string Version = "1.0.0";
-    public const string Name = "MarbleMonkeyMod";
+    public const string WorksOnVersion = "53.0";
+    public const string Version = "1.0.1";
+    public const string Name = "MarbleMonkey";
 
     public const string Description = "Adds a new tower to the game: The Marble Monkey!";
 
